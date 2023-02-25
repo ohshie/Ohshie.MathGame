4c65d08a8b9104d23cb353a88dcd51cba6887094
ohshie <114024290+ohshie@users.noreply.github.com>
2023-02-25 10:03:33 +0400

    More objects and Float check! Rewriten a lot of code to accomodate better use of objects. Now there is no need to "load defauls" as every game mode creates it's own instance GameController to play the game. Also obided to the task requirements and made every answer into an integer.

--- OhshieMathGame/InfiniteGameModeSettings/MaxNumberConfiguration.cs
+++ OhshieMathGame/InfiniteGameModeSettings/MaxNumberConfiguration.cs
@@ -2,13 +2,8 @@ namespace OhshieMathGame.InfiniteGameModeSettings;
 
 public class MaxNumberConfiguration
 {
-    private static int _tempMaxNumber = 11;
-    
-    public static void SaveOperatorConfiguration()
-    {
-        GameController.MaxNumber = _tempMaxNumber;
-    }
-    
+    public static int _tempMaxNumber = 11;
+
     // method to adjust how big numbers can be in equations
     public static void AdjustMaximumAllowedNumber()
     {
@@ -18,14 +13,13 @@ public class MaxNumberConfiguration
         while (true)
         {
             _tempMaxNumber = int.Parse(Console.ReadLine());
-            if (_tempMaxNumber > 0)
-            {
-                break;
-            }
-            Console.WriteLine("Either you entered number that equals or below 0, or not a number at all.");
-            
             // need this adjustment as "random" range does not include actuall last number in range
             _tempMaxNumber += 1;
+            if (_tempMaxNumber > 0)
+            {
+                return;
+            }
+            Console.WriteLine("Either you entered number that equals or below 0, or not a number at all.");
         }
     }
 }
